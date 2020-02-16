c714faf3122575f4465eb0c8958ac5bff50acde2
Nikola Popovski <popovski.nikola@gmail.com>
2020-02-16 23:26:47 +0100

    Initial comit of the Soap Playground project, this project is used for learning and working with SOAP WS

---
+++ soap-playground/src/main/java/mk/codefactory/soap/soapplayground/SoapPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package mk.codefactory.soap.soapplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapPlaygroundApplication.class, args);
+	}
+
+}
